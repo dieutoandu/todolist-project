feac916178fd9516b9b720182b76a7a6e0d53cfa
dieutoandu <dieutoandu@gmail.com>
2025-05-07 21:35:26 +0800

    test_number

--- app/views.py
+++ app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import random
+
+
+def lotto(request):
+    numbers = sorted(random.sample(range(1, 50), 6))
+    spec_number = random.randint(1, 49)
+
+    numbers = " ".join([str(i) for i in numbers])
+    # numbers = " ".join(map(str, numbers))
+
+    result = {"number": numbers, "spec_number": spec_number}
+
+    return render(request, "lotto.html", result)
 
 
 # Create your views here.
 def hello(request):
     result = {"message": "test", "data": "123", "label": "label"}
-
     return JsonResponse(result)
