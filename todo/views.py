c5830b27eac3306179d4e79d1316e4e1dc8b6b50
dieutoandu <dieutoandu@gmail.com>
2025-05-09 21:38:37 +0800

    5_9

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,16 @@ from .models import Todo
 # Create your views here.
 def todolist(request):
     todos = Todo.objects.all()
+    # todos = Todo.objects.filter(id=1)
     print(todos)
     result = {"todos": todos}
     return render(request, "todo/todolist.html", result)
+
+
+def viewtodo(request, id):
+    todo = None
+    try:
+        todo = Todo.objects.get(id=id)
+    except Exception as e:
+        print(e)
+    return render(request, "todo/viewtodo.html", {"todo": todo})
