7e8340ae8d8ea00bf2870ee97b405c087ec96ed1
dieutoandu <dieutoandu@gmail.com>
2025-05-23 20:39:40 +0800

    updata-css

--- todo/forms.py
+++ todo/forms.py
@@ -5,4 +5,4 @@ from .models import Todo
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
-        fields = ["title", "test", "important", "completed"]
+        fields = ["title", "text", "important", "completed"]
--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=100)
-    test = models.TextField(blank=True)
+    text = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     data_completed = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
