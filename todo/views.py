0eb5dbb95c60139b58223469c3796f36a76f638f
dieutoandu <dieutoandu@gmail.com>
2025-05-23 19:35:08 +0800

    2025-5.23_7_35

--- todo/views.py
+++ todo/views.py
@@ -6,22 +6,29 @@ from datetime import datetime
 
 # Create your views here.
 def todolist(request):
+
     user = request.user
     todos = None
     if user.is_authenticated:
+        filter_params = request.GET.get("filter")
+
         todos = Todo.objects.filter(user=request.user).order_by("-created")
+        if filter_params == "important":
+            todos = todos.filter(important=True)
+        elif filter_params == "pending":
+            todos = todos.filter(completed=False)
+        elif filter_params == "completed":
+            todos = todos.filter(completed=True)
+
     print(todos)
     result = {"todos": todos, "user": user}
     return render(request, "todo/todolist.html", result)
 
 
-def deletetodo(request,id):
+def deletetodo(request, id):
     todo = Todo.objects.get(id=id)
     todo.delete()
     return redirect("todolist")
-
-
-
 
 
 def viewtodo(request, id):
@@ -35,19 +42,18 @@ def viewtodo(request, id):
             form = TodoForm(instance=todo)
         else:
             form = TodoForm(request.POST, instance=todo)
-            todo=form.save(commit=False)
+            todo = form.save(commit=False)
             if todo.completed:
                 todo.data_completed = datetime.now().strftime("%Y-%m-%d %H:%M")
             else:
-                todo.data_completed= None
+                todo.data_completed = None
             todo.save()
             massage = "updata pass !"
-
 
     except Exception as e:
         print(e)
     return render(
-        request, "todo/viewtodo.html", {"todo": todo, "form": form, "massage": massage }
+        request, "todo/viewtodo.html", {"todo": todo, "form": form, "massage": massage}
     )
 
 
@@ -56,12 +62,13 @@ def createtodo(request):
         form = TodoForm(request.POST)
         todo = form.save(commit=False)
         todo.user = request.user
+        if todo.completed:
+            todo.data_completed = datetime.now().strftime("%Y-%m-%d %H:%M")
+        else:
+            todo.data_completed = None
         todo.save()
         return redirect("todolist")
     else:
         form = TodoForm()
 
     return render(request, "todo/createtodo.html", {"form": form})
-
-
-
